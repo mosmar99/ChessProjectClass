949ee0815e3f97494d553acdb8f638f2383ab3aa
Mahmut Osmanovic <mahmut.osmanovic@outlook.com>
2023-01-19 12:41:47 +0100

    initial files

---
+++ include/cli.h
@@ -0,0 +1,4 @@
+#ifndef CLI_H_
+#define CLI_H_
+
+#endif
---
+++ src/cli.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("hello world");
+    // gcc -o bin/main.exe src/cli.c
+
+    // C:\Program Files\Git\git-bash
+    return 0;
+}
