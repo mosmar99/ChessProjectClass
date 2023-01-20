cf68a5fb6fc4ecc5b6a0e2bfa49156256c61678a
Linkan42 <sali20jt@student.ju.se>
2023-01-20 22:17:48 +0100

    Fixed extraction of coordinates

--- src/pawn.c
+++ src/pawn.c
@@ -18,27 +18,45 @@ typedef struct Point
 // Returns: The number of occurrenses found, or -1 if invalid parameter(s).
 static int countCharInString(const char const *str, const char c);
 
+// Get a copy of a string.
+//      str: the string to copy
+// Returns: a string.
+static char *getCopyOfString(const char *const str);
+
 // Extract x and y coordinates from a string input.
 //      coord: the string input
 // Returns: A Point struct containing x and y coordinates.
 static Point *extractCoord(const char *const coord);
 
-// Get the two coordinates from input
-//      from: assigned to extracted from coordinate
-//      to: assigned to extracted to coordinate
-static void getCoordinates(const char *const input, Point *const from, Point *const to);
-
 bool *checkPawnMove(const char *const input, char *board[8][8])
 {
-    // check basic validity of parameters
+    // check for basic validity of parameters
     if (input == NULL || board == NULL || strlen(input) != 5)
         return NULL;
     if (countCharInString(input, ' ') != 1)
         return NULL;
 
-    Point *fromCoord, *toCoord;
-    getCoordinates(input, fromCoord, toCoord);
-    assert(fromCoord != NULL && toCoord != NULL);
+    // make a copy of input
+    char *inputCopy = getCopyOfString(input);
+
+    // split input into two sub strings
+    const char const *firstCoord = strtok(inputCopy, " ");
+    const char const *secondCoord = strtok(NULL, " ");
+    assert(firstCoord != NULL && secondCoord != NULL);
+
+    // extract x and y coordinates from the sub strings
+    Point *from = extractCoord(firstCoord);
+    Point *to = extractCoord(secondCoord);
+
+    // check for correct format on desired move
+    if (from == NULL || to == NULL)
+        return NULL;
+
+    // at this point the desired move is correctly formatted and converted to usable data
+    // begin check if the move is valid to make
+
+    // release memory
+    free(inputCopy);
 }
 
 static int countCharInString(const char const *str, const char c)
@@ -52,6 +70,19 @@ static int countCharInString(const char const *str, const char c)
             counter++;
 
     return counter;
+}
+
+static char *getCopyOfString(const char *const str)
+{
+    assert(str != NULL);
+
+    // +1 for null termimator
+    char *newStr = calloc(strlen(str) + 1, sizeof(char));
+    assert(newStr != NULL);
+
+    strncpy(newStr, str, strlen(str));
+
+    return newStr;
 }
 
 static Point *extractCoord(const char *const coord)
@@ -75,29 +106,5 @@ static Point *extractCoord(const char *const coord)
     // flip y coord
     p->y = 8 - p->y;
 
-    // match coordinates to match chess grid
-    p->x++;
-    p->y++;
-
     return p;
-}
-
-static void getCoordinates(const char *const input, Point *from, Point *to)
-{
-    // make a copy of input
-    char *inputCopy = calloc(strlen(input) + 1, sizeof(char));
-    assert(inputCopy != NULL);
-    strncpy(inputCopy, input, strlen(input) + 1);
-
-    // split input into two sub strings
-    char *firstCoord = strtok(inputCopy, " ");
-    char *secondCoord = strtok(NULL, " ");
-    assert(firstCoord != NULL && secondCoord != NULL);
-
-    // extract x and y coordinates from the sub strings
-    Point *fromCoord = extractCoord(firstCoord);
-    Point *toCoord = extractCoord(secondCoord);
-
-    from = fromCoord;
-    to = toCoord;
 }
