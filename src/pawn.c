6ecec5b44a9a9c5048570e8d30c2ac13c71cefd2
Linkan42 <sali20jt@student.ju.se>
2023-01-20 22:40:55 +0100

    Rearranged and cleaned up some shite

--- src/pawn.c
+++ src/pawn.c
@@ -9,19 +9,29 @@
 
 typedef struct Point
 {
-    unsigned int x, y;
+    unsigned short x, y;
 } Point;
+
+typedef struct PointPair
+{
+    Point *to, *from;
+} PointPair;
 
 // Count the number of occurrenses of a specific character in a given string.
 //      str: the string to search
 //      c: the character to count
-// Returns: The number of occurrenses found, or -1 if invalid parameter(s).
-static int countCharInString(const char const *str, const char c);
+// Returns: The number of occurrenses found.
+static int countCharInString(const char const *str, const unsigned char c);
 
 // Get a copy of a string.
 //      str: the string to copy
 // Returns: a string.
 static char *getCopyOfString(const char *const str);
+
+// Get the two imbedded coordinates inside input as a pair of Points.
+//      input: the string input
+// Returns: A PointPair containing the extracted pair of coordinates.
+static PointPair *getCoordinates(const char const *input);
 
 // Extract x and y coordinates from a string input.
 //      coord: the string input
@@ -31,38 +41,22 @@ static Point *extractCoord(const char *const coord);
 bool *checkPawnMove(const char *const input, char *board[8][8])
 {
     // check for basic validity of parameters
-    if (input == NULL || board == NULL || strlen(input) != 5)
-        return NULL;
-    if (countCharInString(input, ' ') != 1)
-        return NULL;
-
-    // make a copy of input
-    char *inputCopy = getCopyOfString(input);
-
-    // split input into two sub strings
-    const char const *firstCoord = strtok(inputCopy, " ");
-    const char const *secondCoord = strtok(NULL, " ");
-    assert(firstCoord != NULL && secondCoord != NULL);
-
-    // extract x and y coordinates from the sub strings
-    Point *from = extractCoord(firstCoord);
-    Point *to = extractCoord(secondCoord);
-
-    // check for correct format on desired move
-    if (from == NULL || to == NULL)
+    assert(input != NULL && board != NULL);
+    if (countCharInString(input, ' ') != 1 || strlen(input) != 5)
         return NULL;
 
-    // at this point the desired move is correctly formatted and converted to usable data
-    // begin check if the move is valid to make
+    PointPair *pair = getCoordinates(input);
+    assert(pair != NULL);
+    if (pair->from == NULL || pair->to == NULL)
+        return NULL;
 
-    // release memory
-    free(inputCopy);
+    // at this point the desired move is correctly formatted and converted to workable data
+    // begin check if the desired move is valid
 }
 
-static int countCharInString(const char const *str, const char c)
+static int countCharInString(const char const *str, const unsigned char c)
 {
-    if (str == NULL)
-        return -1;
+    assert(str != NULL);
 
     int counter = 0;
     for (const char *ptr = str; *ptr != 0; ptr++)
@@ -83,6 +77,34 @@ static char *getCopyOfString(const char *const str)
     strncpy(newStr, str, strlen(str));
 
     return newStr;
+}
+
+static PointPair *getCoordinates(const char const *input)
+{
+    assert(input != NULL);
+
+    // make a copy of input
+    char *inputCopy = getCopyOfString(input);
+
+    // split input into two sub strings
+    const char const *firstCoord = strtok(inputCopy, " ");
+    const char const *secondCoord = strtok(NULL, " ");
+    assert(firstCoord != NULL && secondCoord != NULL);
+
+    // extract x and y coordinates from the sub strings
+    Point *from = extractCoord(firstCoord);
+    Point *to = extractCoord(secondCoord);
+
+    // release memory
+    free(inputCopy);
+
+    // put the twin points together
+    PointPair *pair = calloc(1, sizeof(PointPair));
+    assert(pair != NULL);
+    pair->from = from;
+    pair->to = to;
+
+    return pair;
 }
 
 static Point *extractCoord(const char *const coord)
