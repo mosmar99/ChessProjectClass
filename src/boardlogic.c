538f4d6cbc3fbf1069b89036a98372b353bed253
Mahmut Osmanovic <mahmut.osmanovic@outlook.com>
2023-01-23 22:12:22 +0100

    generalized action move

--- src/boardlogic.c
+++ src/boardlogic.c
@@ -1,85 +1,85 @@
-// #include <stdbool.h>
-// #include <string.h>
-// #include <ctype.h>
-// #include "boardlogic.h"
-// #include "board.h"
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+#include "boardlogic.h"
+#include "board.h"
 
-// struct point{
-//     unsigned int x;
-//     unsigned int y;
-// };
+struct point{
+    unsigned int x;
+    unsigned int y;
+};
 
-// struct move{
-//     char movingPiece;
-//     struct point *fromPoint;
+struct move{
+    char movingPiece;
+    struct point *fromPoint;
 
-//     char capturedPiece;
-//     struct point *toPoint;
-// };
+    char capturedPiece;
+    struct point *toPoint;
+};
 
-// bool makeMove(/*pointer to board + move*/ ){
+bool makeMove(/*pointer to board + move*/ ){
 
-// }
+}
 
-// // move input string ex "Nf3 Nc6", "Nf3 c6"
-// bool parseMove(/*Board array ** to pass along to _CanMove() */char *string, struct move *move){
+// move input string ex "Nf3 Nc6", "Nf3 c6"
+bool parseMove(/*Board array ** to pass along to _CanMove() */char *string, struct move *move){
 
-//     char *token = strtok(string, ' ');
+    char *token = strtok(string, ' ');
 
-//     parseToken(token, true, move->fromPoint, (*move).movingPiece);
+    parseToken(token, true, move->fromPoint, (*move).movingPiece);
 
-//     strtok(NULL, ' ');
+    strtok(NULL, ' ');
 
-//     parseToken(token, false, move->toPoint, (*move).capturedPiece);
+    parseToken(token, false, move->toPoint, (*move).capturedPiece);
 
-//     //send to correct library/function
+    //send to correct library/function
 
-//     //return if move was performed
-//     return true;
-// }
+    //return if move was performed
+    return true;
+}
 
-// bool parseToken(char *token, bool first, struct point *p, char *c){
+bool parseToken(char *token, bool first, struct point *p, char *c){
 
-//     if(!token || strlen(token) < 2 || strlen(token) > 3){ 
-//         return false;
-//     }
+    if(!token || strlen(token) < 2 || strlen(token) > 3){ 
+        return false;
+    }
 
-//     int tokenLength = strlen(token);
+    int tokenLength = strlen(token);
 
-//     // Check and assign rank and file (COL and ROW)
-//     if (charIsValid(token[0], COLINDEX, ROWINDEX) && charIsValid(token[1], ROWINDEX, NUMALLOWEDCHARS)){
-//         p->x = token[tokenLength - 2] - COLOFFSET;
-//         p->y = token[tokenLength - 1] - ROWOFFSET;
-//     } else {
-//         return false;
-//     }
+    // Check and assign rank and file (COL and ROW)
+    if (charIsValid(token[0], COLINDEX, ROWINDEX) && charIsValid(token[1], ROWINDEX, NUMALLOWEDCHARS)){
+        p->x = token[tokenLength - 2] - COLOFFSET;
+        p->y = token[tokenLength - 1] - ROWOFFSET;
+    } else {
+        return false;
+    }
 
-//     // Check that the correct Piece is on the board location
-//     char piece = getPiece(p->x, p->y);
-//     if(tokenLength == 2 && first && (piece == 'p' || piece == 'P')){
-//         *c = piece;
-//         return true;
+    // Check that the correct Piece is on the board location
+    char piece = getPiece(p->x, p->y);
+    if(tokenLength == 2 && first && (piece == 'p' || piece == 'P')){
+        *c = piece;
+        return true;
 
-//     } else if (tokenLength == 2 && !first && (piece == 'p' || piece == 'P' || piece == '-')){
-//         *c = piece;
-//         return true;
+    } else if (tokenLength == 2 && !first && (piece == 'p' || piece == 'P' || piece == '-')){
+        *c = piece;
+        return true;
 
-//     } else if (tokenLength == 3 && (piece == token[0] || piece == token[0] - LOWERCASEOFFSET)){
-//         *c = piece;
-//         return true;
+    } else if (tokenLength == 3 && (piece == token[0] || piece == token[0] - LOWERCASEOFFSET)){
+        *c = piece;
+        return true;
 
-//     } else {
-//         return false;
-//     }  
-// }
+    } else {
+        return false;
+    }  
+}
 
-// bool charIsValid(char *c, int indexStart, int indexStop){
-//     for (size_t i = indexStart; i < indexStop; i++)
-//     {
-//         if (c == ALLOWEDCHARS[i]){
-//             return true;
-//         }           
-//     }
+bool charIsValid(char *c, int indexStart, int indexStop){
+    for (size_t i = indexStart; i < indexStop; i++)
+    {
+        if (c == ALLOWEDCHARS[i]){
+            return true;
+        }           
+    }
 
-//     return false;  
-// }
+    return false;  
+}
--- src/knight.c
+++ src/knight.c
@@ -1,108 +1,108 @@
-#include <string.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include "knight.h"
+// #include <string.h>
+// #include <stdlib.h>
+// #include <ctype.h>
+// #include "knight.h"
 
-typedef struct point{
-    unsigned int x;
-    unsigned int y;
-}point;
+// typedef struct point{
+//     unsigned int x;
+//     unsigned int y;
+// }point;
 
-static bool isSameColour(const char piece1, const char piece2);
-static bool getCoords(char *input, point ** p1, point ** p2);
-static bool parseToken(char *token, point **p);
-static point *createPoint(unsigned int x, unsigned int y);
-static void destroyPoint(point *p);
+// static bool isSameColour(const char piece1, const char piece2);
+// static bool getCoords(char *input, point ** p1, point ** p2);
+// static bool parseToken(char *token, point **p);
+// static point *createPoint(unsigned int x, unsigned int y);
+// static void destroyPoint(point *p);
 
-bool checkKnightMove(char *input, const char *board[8][8]){
+// bool checkKnightMove(char *input, const char *board[8][8]){
 
-    const int pattern[8] = {1,2,2,1,-1,-2,-2,-1};
-    int i = 0;
+//     const int pattern[8] = {1,2,2,1,-1,-2,-2,-1};
+//     int i = 0;
 
-    point *fromPoint = NULL;
-    point *toPoint = NULL;
+//     point *fromPoint = NULL;
+//     point *toPoint = NULL;
 
-    if (!getCoords(input, &fromPoint, &toPoint)){
-        destroyPoint(fromPoint);
-        destroyPoint(toPoint);
-        return false;
-    }
+//     if (!getCoords(input, &fromPoint, &toPoint)){
+//         destroyPoint(fromPoint);
+//         destroyPoint(toPoint);
+//         return false;
+//     }
 
-    while (i < 8 && fromPoint && toPoint){
-        if (!isSameColour(board[fromPoint->x][fromPoint->y], board[toPoint->x][toPoint->y])
-            && toPoint->x - fromPoint->x == pattern[i % 8]
-            && toPoint->y - fromPoint->y == pattern[(i+2) % 8]){
-            return true;
-        }
-        i++;
-    }
+//     while (i < 8 && fromPoint && toPoint){
+//         if (!isSameColour(board[fromPoint->x][fromPoint->y], board[toPoint->x][toPoint->y])
+//             && toPoint->x - fromPoint->x == pattern[i % 8]
+//             && toPoint->y - fromPoint->y == pattern[(i+2) % 8]){
+//             return true;
+//         }
+//         i++;
+//     }
 
-    return false; 
-}
+//     return false; 
+// }
 
-static bool isSameColour(const char *piece1, const char *piece2){
+// static bool isSameColour(const char *piece1, const char *piece2){
 
-    if (piece1[0] != piece2[0]){
-        return false;
-    } else {
-        return true;
-    }
+//     if (piece1[0] != piece2[0]){
+//         return false;
+//     } else {
+//         return true;
+//     }
 
-    return false;
-}
+//     return false;
+// }
 
-static bool getCoords(char *input, point ** p1, point ** p2){
+// static bool getCoords(char *input, point ** p1, point ** p2){
 
-    if (!input || strlen(input) != 5){
-        return false;
-    }
+//     if (!input || strlen(input) != 5){
+//         return false;
+//     }
 
-    char *token = strtok(input," ");
-    if (!parseToken(token, p1)){
-        return false;
-    }
+//     char *token = strtok(input," ");
+//     if (!parseToken(token, p1)){
+//         return false;
+//     }
 
-    token = strtok(NULL," ");
+//     token = strtok(NULL," ");
 
-    if (!parseToken(token, p2)){
-        return false;
-    }
+//     if (!parseToken(token, p2)){
+//         return false;
+//     }
 
-    return true;
-}
+//     return true;
+// }
 
-static bool parseToken(char *token, point ** p){
+// static bool parseToken(char *token, point ** p){
 
-    if (!token || strlen(token) != 2)
-    {
-        return false;
-    }
+//     if (!token || strlen(token) != 2)
+//     {
+//         return false;
+//     }
 
-    int x = token[0] - 'a';
-    int y = token[1] - '1';
-    if (y < 8 && x < 8){
-        *p = createPoint(x, y);
-        return true;
-    } else {
-        return false;
-    }
-}
+//     int x = token[0] - 'a';
+//     int y = token[1] - '1';
+//     if (y < 8 && x < 8){
+//         *p = createPoint(x, y);
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
 
-static point *createPoint(unsigned int x, unsigned int y){
+// static point *createPoint(unsigned int x, unsigned int y){
 
-    point *p = malloc(sizeof(point));
+//     point *p = malloc(sizeof(point));
 
-    if (p){
-        p->x = x;
-        p->y = y;
-        return p;
-    } else {
-        return NULL;
-    }
-}
+//     if (p){
+//         p->x = x;
+//         p->y = y;
+//         return p;
+//     } else {
+//         return NULL;
+//     }
+// }
 
-static void destroyPoint(point *p){
-    if (p){
-        free(p);
-    } 
-}
+// static void destroyPoint(point *p){
+//     if (p){
+//         free(p);
+//     } 
+// }
