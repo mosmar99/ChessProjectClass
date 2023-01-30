71c9de8a231a2bc1edd8702a1cd6801d645d062d
Besan Ewir <besanew1999@icloud.com>
2023-01-30 20:54:19 +0100

    added

--- include/knight.h
+++
@@ -1,7 +0,0 @@
-#ifndef KNIGHT_H
-#define KNIGHT_H
-#include <stdbool.h>
-
-bool checkKnightMove(move *move, const char *board[8][8]);
-
-#endif
--- include/rook.h
+++
@@ -1,9 +0,0 @@
-#ifndef _ROOK_H
-#define _ROOK_H
-
-#include <stdbool.h>
-#include "makemove.h"
-
-
-bool checkRookMove(const move *const move, char *const board[8][8]);
-#endif
