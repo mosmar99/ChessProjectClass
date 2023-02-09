6401e4fa3838ddc0e499c88631ed04b755f9b350
Besan Ewir <besanew1999@icloud.com>
2023-02-09 01:48:45 +0100

    adding jump test

--- test/testRook.c
+++ test/testRook.c
@@ -5,32 +5,7 @@
 
 
 
-void printBoard(char *board[8][8]) {
-    char numbers[8] = {'1', '2', '3', '4', '5', '6', '7', '8'};
-    char letters[8] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
 
-    printf("\n");
-    for (size_t i = 0; i < 8; i++)
-    {
-        printf("%c   ", *(numbers+(7-i)));
-        for (size_t j = 0; j < 8; j++)
-        {
-            printf("%s  ", board[7-i][j]);
-        }
-        printf("\n");
-    }
-
-    printf("\n    ");
-    fflush(stdout);
-
-    for (size_t i = 0; i < 8; i++)
-    {
-        printf("%c   ", *(letters+i));
-    }
-    
-
-    printf("\n\n");
-}
 static void initBoard(char *board[8][8]) {
     for (size_t i = 0; i < 8; i++)
     {
@@ -54,28 +29,35 @@ static void initBoard(char *board[8][8]) {
 int main() {
     char *board[8][8];
     initBoard(board);
-    board[0][2] = "bp";
-    printBoard(board);
     
-    // move *move1 = createMove(createPoint(0, 0), createPoint(0, 4), "wR", "bp");
-    // assert(checkRookMove(move1, board) == true);
-    // move *move2 = createMove(createPoint(0, 0), createPoint(4, 4), "wR", "bp");
-    // assert(checkRookMove(move2, board) == false);
-    // move *move3 = createMove(createPoint(0, 0), createPoint(1, 0), "wR", "bp");
-    // assert(checkRookMove(move3, board) == true);
-    // move *move4 = createMove(createPoint(0, 0), createPoint(1, 2), "wR", "bp");
-    // assert(checkRookMove(move4, board) == false);
-    // move *move5 = createMove(createPoint(0, 0), createPoint(0, 7), "wR", "wp");
-    // assert(checkRookMove(move5, board) == true);
+    
+    
+    move *move1 = createMove(createPoint(0, 0), createPoint(0, 4), "wR", "bp");
+    assert(checkRookMove(move1, board) == true);
 
-    move *move5 = createMove(createPoint(0, 0), createPoint(3, 0), "wR", "bp");
-    assert(checkRookMove(move5, board) == false);
+    move *move2 = createMove(createPoint(0, 0), createPoint(4, 4), "wR", "bp");
+    assert(checkRookMove(move2, board) == false);
 
-    // destroyMove(move1);
-    // destroyMove(move2);
-    // destroyMove(move3);
-    // destroyMove(move4);
-    // destroyMove(move5);
+    move *move3 = createMove(createPoint(0, 0), createPoint(1, 0), "wR", "bp");
+    assert(checkRookMove(move3, board) == true);
+
+    move *move4 = createMove(createPoint(0, 0), createPoint(1, 2), "wR", "bp");
+    assert(checkRookMove(move4, board) == false);
+
+    move *move5 = createMove(createPoint(0, 0), createPoint(0, 7), "wR", "wp");
+    assert(checkRookMove(move5, board) == true);
+    
+    //(testing if it jump)
+    board[0][2] = "bp"; 
+    move *move6 = createMove(createPoint(0, 0), createPoint(3, 0), "wR", "bp");
+    assert(checkRookMove(move6, board) == false); 
+
+    destroyMove(move1);
+    destroyMove(move2);
+    destroyMove(move3);
+    destroyMove(move4);
+    destroyMove(move5);
+    destroyMove(move6);
     
     printf("ROOK TEST PASSED\n");
 
