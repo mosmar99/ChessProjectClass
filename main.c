3eedf11c11c2731c40af703878c7a05f3fe6e3f0
mosmar99 <mahmut.osmanovic@outlook.com>
2023-02-26 13:34:04 +0100

    edited string

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main(void) {
     printf(BCYN "\n%s\n\n", "### New Game ###  " reset);
     printf(UYEL "%s\n", "Move Format:" reset);
     printf("%s%s%s\n", YEL "--> Pawns: " reset, "<From Column><From Row>--<Space>--<To Column><To Row>", GRN " e2 e4" reset);
-    printf("%s%s%s\n", YEL "--> Minor or Major Pieces: " reset, "<Piece Letter><From Column><From Row>--<Space>--<Piece latter><To Column><To Row>", GRN " Nb1 Nc3" reset);
+    printf("%s%s%s\n", YEL "--> Minor or Major Pieces: " reset, "<Piece Letter><From Column><From Row>--<Space>--<Piece Letter><To Column><To Row>", GRN " Nb1 Nc3" reset);
     char *board[8][8];
     initBoard(board);
     printBoard(board);
