5cd248c2f74d61d105f3670a23318a39b4c42700
mosmar99 <mahmut.osmanovic@outlook.com>
2023-03-02 16:07:14 +0100

    put in normal chess board

--- src/makeBoard.c
+++ src/makeBoard.c
@@ -88,38 +88,38 @@ void initBoard(char *board[8][8]) {
     }
 
     // white
-    // board[0][0] = "wR";
-    // board[0][1] = "wN";
-    // board[0][2] = "wB";
+    board[0][0] = "wR";
+    board[0][1] = "wN";
+    board[0][2] = "wB";
     board[0][3] = "wQ";
     board[0][4] = "wK";
-    // board[0][5] = "wB";
-    // board[0][6] = "wN";
-    // board[0][7] = "wR";
-    // board[1][0] = "wp";
-    // board[1][1] = "wp";
-    // board[1][2] = "wp";
-    // board[1][3] = "wp";
-    // board[1][4] = "wp";
-    // board[1][5] = "wp";
-    // board[1][6] = "wp";
-    // board[1][7] = "wp";
+    board[0][5] = "wB";
+    board[0][6] = "wN";
+    board[0][7] = "wR";
+    board[1][0] = "wp";
+    board[1][1] = "wp";
+    board[1][2] = "wp";
+    board[1][3] = "wp";
+    board[1][4] = "wp";
+    board[1][5] = "wp";
+    board[1][6] = "wp";
+    board[1][7] = "wp";
 
     // black
-    // board[7][0] = "bR";
-    // board[7][1] = "bN";
-    // board[7][2] = "bB";
-    // board[7][3] = "bQ";
+    board[7][0] = "bR";
+    board[7][1] = "bN";
+    board[7][2] = "bB";
+    board[7][3] = "bQ";
     board[7][4] = "bK";
-    // board[7][5] = "bB";
-    // board[7][6] = "bN";
-    // board[7][7] = "bR";
-    // board[6][0] = "bp";
-    // board[6][1] = "bp";
-    // board[6][2] = "bp";
-    // board[6][3] = "bp";
-    // board[6][4] = "bp";
-    // board[6][5] = "bp";
-    // board[6][6] = "bp";
-    // board[6][7] = "bp";
+    board[7][5] = "bB";
+    board[7][6] = "bN";
+    board[7][7] = "bR";
+    board[6][0] = "bp";
+    board[6][1] = "bp";
+    board[6][2] = "bp";
+    board[6][3] = "bp";
+    board[6][4] = "bp";
+    board[6][5] = "bp";
+    board[6][6] = "bp";
+    board[6][7] = "bp";
 }
