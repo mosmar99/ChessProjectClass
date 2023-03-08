cf73ad162413044cdaadd854153956cdcbbfa350
Isac Paulsson <isac.97@live.se>
2023-03-08 17:20:39 +0100

    implemented rudimentary gui

--- python/chessWrapper.py
+++ python/chessWrapper.py
@@ -3,24 +3,31 @@ import ctypes as ct
 
 chessLib = ct.CDLL("./bin/chess.dll")
 
-chessLib.initGame()
+class chessGame():
 
-arr = ((ct.c_char_p * 8) * 8)()
-chessLib.readBoard(arr)
+    def __init__(self):
+        chessLib.initGame()
+        self.board = [["--"]*8 for i in range(8)]
+        self.updateBoard()
 
-def printboard(arr):
-    for row in range(8):
+    def updateBoard(self):
+        arr = ((ct.c_char_p * 8) * 8)()
+        chessLib.readBoard(arr)
+        for x in range(8):
+            for y in range(8):
+                self.board[x][y] = arr[x][y].decode("utf-8")
+
+    def tryTurn(self, fx, fy, tx, ty, mp, cp):
+        return int(chessLib.tryTurn(fx, fy, tx, ty, mp.encode(), cp.encode()))
+
+    def printBoard(self):
+        for row in range(8):
+            print()
+            for col in range(8):
+                print(self.arr[7-row][7-col].decode(), end = " ")
         print()
-        for col in range(8):
-            print(arr[7-row][7-col].decode(), end = " ")
-    print()
 
-printboard(arr)
-chessLib.tryTurn(1,1,1,2,"wp".encode(),"--".encode())
-chessLib.readBoard(arr)
-printboard(arr)
+    def getPiece(self,x,y) -> str:
+        return self.board[x][y]
 
-chessLib.tryTurn(1,6,1,5,"bp".encode(),"--".encode())
-chessLib.readBoard(arr)
-printboard(arr)
 
