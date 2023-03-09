20bbfffe25fa029c44058bb7cdf749f09a6f9d0f
Sebastian Tuura <tuura.sebastian@gmail.com>
2023-03-09 01:38:31 +0100

    test

--- tests/unitTests/utCheckmate.c
+++ tests/unitTests/utCheckmate.c
@@ -12,85 +12,81 @@ void main (void){
     char * board[8][8];
     char * flag = malloc(sizeof(char[50]));
 
-    #pragma region ***non-checkmates, but checks***
-        baseHistory(3);
-        baseBoard(board);
-        flag = "reset";
+    baseHistory(2);
+    baseBoard(board);
+    flag = "reset";
 
-        board[7][7] = "wK";
-        board[0][7] = "bQ";
-        checkmate(board, hs, &flag);
-        assert(strcmp(flag, "CHECK") == 0);
+    board[7][7] = "wK";
+    board[0][7] = "bQ";
+    
+    checkmate(board, hs, &flag);
+    assert(strcmp(flag, "CHECK") == 0);
 
-        flag = "reset";
-        baseHistory(2);
-        baseBoard(board);
+    flag = "reset";
+    baseHistory(2);
+    baseBoard(board);
 
-        board[7][7] = "wK";
-        board[5][6] = "bN";
-        checkmate(board, hs, &flag);
-        assert(strcmp(flag, "CHECK") == 0);
-    #pragma endregion
+    board[7][7] = "wK";
+    board[5][6] = "bN";
+    checkmate(board, hs, &flag);
+    assert(strcmp(flag, "CHECK") == 0);
 
-    #pragma region ***non-checkmates, but checks where the king is unable to move, but a friendly piece can save the king***
-        baseHistory(2);
-        baseBoard(board);
-        flag = "reset";
+    baseHistory(2);
+    baseBoard(board);
+    flag = "reset";
 
-        board[0][7] = "wK";
-        board[1][7] = "wp";
-        board[1][6] = "wp";
-        board[7][7] = "wB"; //bishop can attack queen diagonally h8-a1
-        board[0][0] = "bQ";
-        checkmate(board, hs, &flag);
-        assert(strcmp(flag, "CHECK") == 0);
+    board[0][7] = "wK";
+    board[1][7] = "wp";
+    board[1][6] = "wp";
+    board[7][7] = "wB"; //bishop can attack queen diagonally h8-a1
+    board[0][0] = "bQ";
+    checkmate(board, hs, &flag);
+    assert(strcmp(flag, "CHECK") == 0);
 
-        baseHistory(2);
-        baseBoard(board);
-        flag = "reset";
+    baseHistory(2);
+    baseBoard(board);
+    flag = "reset";
 
-        board[7][7] = "wK";
-        board[6][7] = "wp";
-        board[6][6] = "wp"; //this pawn can move up one
-        board[7][0] = "bQ";
-        checkmate(board, hs, &flag);
-        assert(strcmp(flag, "CHECK") == 0);
-    #pragma endregion
+    board[7][7] = "wK";
+    board[6][7] = "wp";
+    board[6][6] = "wp"; //this pawn can move up one
+    board[7][0] = "bQ";
+    checkmate(board, hs, &flag);
+    assert(strcmp(flag, "CHECK") == 0);
 
-    #pragma region ***checkmates***
-        baseHistory(2);
-        baseBoard(board);
-        flag = "reset";
 
-        board[0][7] = "wK"; //same position as the bishop one above but this time no bishop to save the king.
-        board[1][7] = "wp";
-        board[1][6] = "wp";
-        board[0][0] = "bQ";
-        checkmate(board, hs, &flag);
-        assert(strcmp(flag, "CHECKMATE") == 0);
+    baseHistory(2);
+    baseBoard(board);
+    flag = "reset";
 
-        baseHistory(2);
-        baseBoard(board);
-        flag = "reset";
+    board[0][7] = "wK"; //same position as the bishop one above but this time no bishop to save the king.
+    board[1][7] = "wp";
+    board[1][6] = "wp";
+    board[0][0] = "bQ";
+    checkmate(board, hs, &flag);
+    assert(strcmp(flag, "CHECKMATE") == 0);
 
-        board[0][7] = "wK";
-        board[1][7] = "wp";
-        board[1][6] = "wp";
-        board[7][7] = "wN"; //add random knight that cant save the king
-        board[0][0] = "bQ";
-        checkmate(board, hs, &flag);
-        assert(strcmp(flag, "CHECKMATE") == 0);
+    baseHistory(2);
+    baseBoard(board);
+    flag = "reset";
 
-        baseHistory(2);
-        baseBoard(board);
-        flag = "reset";
+    board[0][7] = "wK";
+    board[1][7] = "wp";
+    board[1][6] = "wp";
+    board[7][7] = "wN"; //add random knight that cant save the king
+    board[0][0] = "bQ";
+    checkmate(board, hs, &flag);
+    assert(strcmp(flag, "CHECKMATE") == 0);
 
-        board[0][7] = "wK";
-        board[1][0] = "bQ";
-        board[0][0] = "bQ";
-        checkmate(board, hs, &flag);
-        assert(strcmp(flag, "CHECKMATE") == 0);
-    #pragma endregion
+    baseHistory(2);
+    baseBoard(board);
+    flag = "reset";
+
+    board[0][7] = "wK";
+    board[1][0] = "bQ";
+    board[0][0] = "bQ";
+    checkmate(board, hs, &flag);
+    assert(strcmp(flag, "CHECKMATE") == 0);
 
     printf("\nCHECKMATE TEST PASSED\n");
     system("pause");
